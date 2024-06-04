1c6c7ba91548af3636335919cdbfbad601e5ecf7
capGoblin <dharshan2457@gmail.com>
2024-06-04 21:51:19 +0530

    rename contracts

--- packages/hardhat/ignition/modules/SkillVouch.ts
+++ packages/hardhat/ignition/modules/SkillVouch.ts
@@ -2,7 +2,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("SkillVouch", (m) => {
   const tokenContract = m.contract("TokenContract", []);
-  const skillVouchState = m.contract("UserRequestStruct", [tokenContract]);
+  const skillVouchState = m.contract("SkillVouchState", [tokenContract]);
   const skillVouchContract = m.contract("SkillVouchContract", [
     skillVouchState,
   ]);
