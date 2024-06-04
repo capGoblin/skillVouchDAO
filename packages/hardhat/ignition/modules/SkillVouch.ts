bcb69f99dc357891b46d81808ced441bda1ec449
capGoblin <dharshan2457@gmail.com>
2024-06-04 21:45:15 +0530

    update buildModule

--- packages/hardhat/ignition/modules/SkillVouch.ts
+++ packages/hardhat/ignition/modules/SkillVouch.ts
@@ -3,23 +3,13 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 export default buildModule("SkillVouch", (m) => {
   const tokenContract = m.contract("TokenContract", []);
   const userRequestStruct = m.contract("UserRequestStruct", [tokenContract]);
-  const userRequestContract = m.contract("UserRequestContract", [
+  const skillVouchContract = m.contract("SkillVouchContract", [
     userRequestStruct,
   ]);
-  const vouchingProcessContract = m.contract("VouchingProcessContract", [
-    userRequestContract,
-    userRequestStruct,
-  ]);
-  const communityValidationContract = m.contract(
-    "CommunityValidationContract",
-    [userRequestContract, userRequestStruct]
-  );
 
   return {
     tokenContract,
     userRequestStruct,
-    userRequestContract,
-    vouchingProcessContract,
-    communityValidationContract,
+    skillVouchContract,
   };
 });
