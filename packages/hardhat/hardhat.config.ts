ce3aa896d17d10dcc1e12ef969a96ff3df2e78c3
capGoblin <dharshan2457@gmail.com>
2024-06-05 21:19:35 +0530

    Initialize subgraph

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -9,16 +9,15 @@ const config: any = {
       url: process.env.NETWORK_URL,
       accounts: [process.env.PRIVATE_KEY],
     },
-    // etherscan: {
-    //   // Your API key for Etherscan
-    //   // Obtain one at https://etherscan.io/
-    //   apiKey: process.env.ETHERSCAN_API_KEY,
-    // },
+
     // sourcify: {
     //   // Disabled by default
     //   // Doesn't need an API key
     //   enabled: true,
     // },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
 
